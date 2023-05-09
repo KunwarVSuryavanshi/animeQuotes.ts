dd1477e7bb6eb17e84d8c3b3dc9f7b0bd163002c
KunwarVSuryavanshi <virajkunwar98@gmail.com>
2023-05-09 17:14:18 +0530

    :confetti_ball: basic things

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,32 @@
 import quotes from './quotes';
 
-const randomQuote = quotes[Math.floor(Math.random() * quotes.length)];
+type QuotesKey = 'anime' | 'name';
+
+const searchQuotes = (key: QuotesKey, value: string, limit?: number) => {
+  let quotesArr = [];
+  for (let i = 0; i < quotes.length; i++) {
+    if (quotes[i][key].toLowerCase() === value.toLowerCase()) {
+      quotesArr.push(quotes[i]);
+    }
+  }
+  if (!limit)
+    return quotesArr[Math.floor(Math.random() * quotesArr.length)];
+  return quotesArr.slice(0, limit < quotesArr.length && limit > 0 ? limit : quotesArr.length);
+}
+
+const randomQuote = (limit?: number) => {
+  if (!limit) return quotes[Math.floor(Math.random() * quotes.length)];
+  return quotes.slice(0, limit < quotes.length && limit > 0 ? limit : quotes.length);
+}
+
+const randomQuoteByAnime = (anime: string, limit?: number) => {
+  const animeQuotes = searchQuotes('anime', anime, limit);
+  return animeQuotes;
+}
+
+const randomQuoteByCharacter = (character: string, limit?: number) => {
+  const charQuotes = searchQuotes('name', character, limit);
+  return charQuotes;
+}
+
+console.log(randomQuote(), randomQuoteByAnime('Naruto'), randomQuoteByCharacter('Naruto'));
